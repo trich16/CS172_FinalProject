7ca1cc8bffd32d39245e8cd1e9fb67aea5d784a7
mkeiper <mkeiper14@my.whitworth.edu>
2014-11-29 10:35:37 -0800

    Overhaul1 - It works!
    Added several function calls and variables.

--- CS172_FinalProject/pipe.h
+++ CS172_FinalProject/pipe.h
@@ -1,23 +1,34 @@
 #ifndef PIPE_H
 #define PIPE_H
 
-#include <String>
-
 class Pipe
 {
   private:
     int pinNumber;
-    double waterFreq
-    double waterLength
-    string tempPin;
-    string humPin;
+    long waterFreq;
+    long prevFreq;
+    long waterLength;
+    long prevLength;
+    int pumpSet;
+    int tempPin;
+    int humPin;
   public:
     void setPinNumber(int i);
     int getPinNumber();
     void turnOn();
     void turnOff();
-    void setWaterTime();
-    void getWaterTime();
+    void setWaterFreq(int i);
+    int getWaterFreq();
+    void setWaterLength(int i);
+    void checkFreq();
+    void setPumpSet();
+    int getPumpSet();
+    void setTempPin(int i);
+    int getTempPin();
+    void setHumPin(int i);
+    int getHumPin();
+    //need constructor
+    void setPrevFreq(int i);
 };
 
 #endif
