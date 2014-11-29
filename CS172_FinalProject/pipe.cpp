88b30b9066f32352d9b10e1aef37f802322ae632
mkeiper <mkeiper14@my.whitworth.edu>
2014-11-29 10:34:30 -0800

    Overhaul1 - It works!
    Added a number of set and get functions.
    Added main function definition checkFreq that checks if it is time to water and then does it.

--- CS172_FinalProject/pipe.cpp
+++ CS172_FinalProject/pipe.cpp
@@ -1,4 +1,16 @@
 #include "pipe.h"
+#include "Arduino.h"
+
+
+void Pipe::setWaterFreq(int i)
+{
+  waterFreq = i;
+}
+
+void Pipe::setWaterLength(int i)
+{
+  waterLength = i;
+}
 
 void Pipe::setPinNumber(int i)
 {
@@ -10,14 +22,83 @@ int Pipe::getPinNumber()
   return pinNumber;  
 }
 
+void Pipe::setTempPin(int i)
+{
+  tempPin = i;
+}
+
+int Pipe::getTempPin()
+{
+  return tempPin;
+}
+
+void Pipe::setHumPin(int i)
+{
+  humPin = i;
+}
+
+int Pipe::getHumPin()
+{
+  return humPin;
+}
+
 void Pipe::turnOn()
 {
   digitalWrite(pinNumber, HIGH);
 }
 
-void Pipe::turnOff(String p)
+void Pipe::turnOff()
 {
   digitalWrite(pinNumber, LOW);
 }
 
+void Pipe::checkFreq()
+{
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - prevFreq > waterFreq){
+    //save last time we watered
+    prevFreq = currentMillis;
+    //turn on watering
+    if (pumpSet == 0){
+       Serial.print(pinNumber); 
+       Serial.print(" Turned On at: ");
+       Serial.print(currentMillis / 1000);
+       Serial.println(" seconds");
+       } 
+    pumpSet = 1;
+    turnOn();
+    }
+  
+  if(currentMillis - prevFreq > waterLength){
+    if(pumpSet == 1){
+      Serial.print(pinNumber);
+      Serial.print(" Turned Off at: ");
+      Serial.print(currentMillis / 1000);
+      Serial.println(" seconds");
+      pumpSet = 0;
+      }
+    turnOff();
+    }
+}
+  
+int Pipe::getPumpSet()
+{
+  return pumpSet;
+}
+
+//need constructor
+void Pipe::setPrevFreq(int i)
+{
+  prevFreq = i;
+}
+
+void Pipe::setPumpSet()
+{
+  pumpSet = 0;
+}
+
+
+    
+    
 
