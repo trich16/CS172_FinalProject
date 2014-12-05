ebdf102cf98f2a41d04567641ee2fe9cba6c0e2f
mkeiper <mkeiper14@my.whitworth.edu>
2014-12-04 22:20:21 -0800

    12/4 working

--- CS172_FinalProject/HydroponicsProgram.ino
+++ CS172_FinalProject/HydroponicsProgram.ino
@@ -1,26 +1,24 @@
 
 #include <SD.h>
-#include <Ethernet.h>
+//#include <Ethernet.h>
 //#include "Twitter.h"
 #include "pipe.h"
-#include <SPI.h>
-#include <Ethernet.h>
-//#include "sha1.h"
-#include "Time.h"
-#include <EEPROM.h>
-
+//#include "waterTime.txt"
 File dataFile;
 const int numberOfPipes = 3;
 int pumpPin = 5;
 int pumpStat = 0;
-char txtNumber[6];
 //create three pipe objects 
+long int txtNumber[6];
 Pipe pipe[3];
+//DEFAULT CONSTRUCTOR
+
 
 void setup()
 {
+
   //open serial communication, initialize SD card
-  Serial.begin(9600);                             // We may need to change this to 115200 due to Twitter if it is slow
+  Serial.begin(9600);
   Serial.print("Initializing SD card...");
   pinMode(10, OUTPUT);
   if (!SD.begin(4)) {
@@ -34,14 +32,23 @@ void setup()
     pipe[0].setPinNumber(2);
     pipe[1].setPinNumber(3);
     pipe[2].setPinNumber(5);
-  
+    
+    pipe[0].setWaterFreq(6000);
+    pipe[1].setWaterFreq(6000);
+    pipe[2].setWaterFreq(6000);
+    
+    pipe[0].setWaterLength(5000);
+    pipe[1].setWaterLength(5000);
+    pipe[2].setWaterLength(5000);
+    
   //initialize pins to output
   for (int i = 0; i < numberOfPipes; i++) 
     pinMode(pipe[i].getPinNumber(), OUTPUT);
     
   
   
-    dataFile = SD.open("test.txt", FILE_READ);
+  
+  dataFile = SD.open("test.txt", FILE_READ);
   if (SD.exists("test.txt")) {
     Serial.print("Opening test.txt...");
   } else {
@@ -49,14 +56,13 @@ void setup()
     Serial.println("error opening test.txt");
   }
   
-  //make a read function to return numbers from the text file
-  if (SD.exists("test.txt")){
+    if (SD.exists("test.txt")){
      Serial.println("Succeeded in opening test.txt");
   for(int i = 0; i < 6; i++){
      char c = dataFile.read();
-     int a;
+     long int a;
      a=c-'0';
-     txtNumber[i] = a;
+     txtNumber[i] = a *1000;
      Serial.println(txtNumber[i]); //output the variable on the screen to verify
     }
    }
@@ -67,19 +73,25 @@ void setup()
   dataFile.close();
 
   for(int i = 0; i < 3; i++){
-    pipe[i].setWaterFreq(txtNumber[i+i]);
-    pipe[i].setWaterLength(txtNumber[i+i+1]); 
+    int f = txtNumber[i+i];
+    int l = txtNumber[i+i+1];
+    pipe[i].setWaterFreq(f);
+    pipe[i].setWaterLength(l);
+    Serial.println(pipe[i].getWaterLength()); 
   }
+
+  
 }
 
 void loop()
 {
-   //loop checks whether it is time to water or not. If it is, it waters for the correct length.
+  //loop checks whether it is time to water or not. If it is, it waters for the correct length.
   for (int i=0; i < numberOfPipes; i++){
     pipe[i].checkFreq();
+   // Serial.println(pipe[i].getWaterFreq());
   }
   
-  
+  // checks to see if any pipes needs the pump to be on and turns the pump on or off accordingly
   if(pipe[0].getPumpSet() == 1 || pipe[1].getPumpSet() == 1 || pipe[2].getPumpSet() == 1){
     if(pumpStat == 0){
       Serial.print("Pump Turned On at: ");
